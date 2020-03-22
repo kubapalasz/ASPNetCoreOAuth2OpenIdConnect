bdc2ff231ce76e11a924f952c0ff8a0dae7de7ea
Jakub Pałaszyński <japa@edc.dk>
2020-03-22 17:07:14 +0100

    Simple way of filtering Images by OwnerId - Authorization

--- src/IDP/Quickstart/TestUsers.cs
+++ src/IDP/Quickstart/TestUsers.cs
@@ -13,7 +13,7 @@ namespace IDP
     {
         public static List<TestUser> Users = new List<TestUser>
         {
-            new TestUser{SubjectId = "818727", Username = "Frank", Password = "password", 
+            new TestUser{SubjectId = "d860efca-22d9-47fd-8249-791ba61b07c7", Username = "Frank", Password = "password", 
                 Claims = 
                 {
                     new Claim(JwtClaimTypes.GivenName, "Frank"),
@@ -22,7 +22,7 @@ namespace IDP
                     new Claim(JwtClaimTypes.Role, "FreeUser"),
                 }
             },
-            new TestUser{SubjectId = "88421113", Username = "Claire", Password = "password", 
+            new TestUser{SubjectId = "b7539694-97e7-4dfe-84da-b4256e1ff5c7", Username = "Claire", Password = "password", 
                 Claims = 
                 {
                     new Claim(JwtClaimTypes.GivenName, "Claire"),
